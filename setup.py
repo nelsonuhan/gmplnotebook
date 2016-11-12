cb81dc2cbb4118d8ed30db443cd1e7e0d524e11e
Nelson Uhan <nelson@uhan.me>
2016-11-11 21:05:58 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gmplnotebook',
-    version='0.1',
+    version='0.1.1',
     description='A GMPL/MathProg kernel+extension for Jupyter',
     url='https://github.com/nelsonuhan/gmplnotebook',
     author='Nelson Uhan',
